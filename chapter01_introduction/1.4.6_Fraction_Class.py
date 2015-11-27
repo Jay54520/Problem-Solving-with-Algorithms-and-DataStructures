1cf1e9cc87b2695f311aacff9d827ceecc11f8a8
Simon <simin_jie@163.com>
2015-11-27 21:10:44 +0800

    __radd__, __iadd__ and _add__; __repr__ and __str__

--- chapter01_introduction/1.4.6_Fraction_Class.py
+++ chapter01_introduction/1.4.6_Fraction_Class.py
@@ -10,43 +10,77 @@ def gcd(m, n):
 
 class Fraction:
     def __init__(self, top, bottom):
-        self.num = top
-        self.den = bottom
+        if not isinstance(top, int) or not isinstance(bottom, int):
+            raise ValueError("The numbers must be int")
+        if bottom == 0:
+            raise ValueError("The bottom must not equal to 0")
+        if top > 0 and bottom < 0:
+            top = -top
+            bottom = -bottom                   
+        common = gcd(top, bottom)
+        self.num = top // common
+        self.den = bottom // common
+    
+    def get_num(self):
+        return self.num
+        
+    def get_den(self):
+        return self.den
     
     def show(self):
         print(self.num, "/", self.den)
     
     # When need to convert to str
     def __str__(self):
-        return str(self.num) + "/" + str(self.den)    
+        return str(self.num) + "/" + str(self.den) 
+    
+    # focus on making __str__ reasonably human-readable, 
+    # and __repr__ as unambiguous as you possibly can
+    def __repr__(self):
+        return "Unambiguous"
     
     # f1 + f2
     def __add__(self, other_fraction):
         new_num = self.num*other_fraction.den + self.den*other_fraction.num 
-        new_den = self.den * other_fraction.den
-        common = gcd(new_num, new_den)
-        return Fraction(new_num // common, new_den // common)
+        new_den = self.den * other_fraction.den        
+        return Fraction(new_num, new_den)
     
+    # x - y , 
+    def __radd__(self, other_fraction):
+        new_num = self.num*other_fraction.den + self.den*other_fraction.num 
+        new_den = self.den * other_fraction.den        
+        return Fraction(new_num, new_den)
+    
+    # x+=y
+    def __iadd__(self, other_fraction):
+        self.num = self.num*other_fraction.den + self.den*other_fraction.num 
+        self.den = self.den * other_fraction.den        
+        return Fraction(self.num, self.den)
+        
     # f1 - f2 
     def __sub__(self, other_fraction):
         new_num = self.num*other_fraction.den-self.den*other_fraction.num 
         new_den = self.den*other_fraction.den
-        common = gcd(new_num, new_den)
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
+    
+    # y is an instance of a class that has an __rsub__() method, 
+    # y.__rsub__(x) is called if x.__sub__(y) returns NotImplemented.
+    def __rsub__(self, other_fraction):
+        new_num = self.num*other_fraction.den-self.den*other_fraction.num 
+        new_den = self.den*other_fraction.den
+        return Fraction(new_num, new_den)
         
     # f1 * f2 
     def __mul__(self, other_fraction):
         new_num = self.num*other_fraction.num 
-        new_den = self.den*other_fraction.den
-        common = gcd(new_num, new_den)
-        return Fraction(new_num // common, new_den // common)
+        new_den = self.den*other_fraction.den        
+        return Fraction(new_num, new_den)
         
     # f1 / f2 
     def __truediv__(self, other_fraction):
         new_num = self.num*other_fraction.den 
-        new_den = self.den*other_fraction.num 
-        common = gcd(new_num, new_den)
-        return Fraction(new_num // common, new_den // common)
+        new_den = self.den*other_fraction.num         
+        return Fraction(new_num, new_den)
         
     # f1 < f2
     def __lt__(self, other_fraction):
@@ -68,9 +102,12 @@ class Fraction:
 
 print(gcd(20, 10))        
 f1 = Fraction(1, 4)
-f2 = Fraction(1, 2)
+f2 = Fraction(4, 8)
 f3 = f1 + f2
 print(f3)
 print(f1 - f2, f1 * f2, f1 / f2, f1 < f2, f1 > f2, f1 == f2)
+print(f2.get_num(), f2.get_den())
+f1 += f2
+print(f1.__repr__())
 
 
