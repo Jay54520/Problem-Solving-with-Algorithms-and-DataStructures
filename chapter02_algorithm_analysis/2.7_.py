66fd71cc5ac9ee98ee15fb56cd864cd9e00235ce
Simon <simin_jie@163.com>
2015-11-28 19:58:00 +0800

    Compares the performance of the del operator on lists and dictionaries

--- chapter02_algorithm_analysis/2.7_.py
+++ chapter02_algorithm_analysis/2.7_.py
@@ -1,5 +1,6 @@
 import timeit
 import random
+import ipdb
 
 for i in range(10000, 1000000, 20000):
     # List index operator
@@ -12,7 +13,7 @@ for i in range(10000, 1000000, 20000):
     
     # Get item for dictionaries
     t = timeit.Timer('x.get(random.randrange(%d))' % i, 'from __main__ import random, x')
-    x = {j:None for j in range(i)}
+    x = {j:None for j in range(i)}    
     dict_get_time = t.timeit(number=1000)
     #print('%d, %10.3f' % (i, dict_get_time))
     
@@ -23,12 +24,13 @@ for i in range(10000, 1000000, 20000):
     #print('%d, %10.3f' % (i, dict_set_time))
     
     # Compares the performance of the del operator on lists and dictionaries
-    t = timeit.Timer('del x[random.randrange(%d)]' % i, 'from __main__ import random, x')
-    x = {j:None for j in range(i)}
+    #x = {j:None for j in range(i)}
+    #为什么这里不能用引入？
+    t = timeit.Timer('del {j:None for j in range(%d)}[random.randrange(%d)]' % (i, i), 'import random')        
     dict_del_time = t.timeit(number=1000)
     
-    t = timeit.Timer('del x[random.randrange(%d)]' % (i), 'from __main__ import random, x')
-    x = list(range(i))
+    t = timeit.Timer('del list(range(%d))[random.randrange(%d)]' % (i, i), 'from __main__ import random, x')
+    #x = list(range(i))
     list_del_time = t.timeit(number=1000)
     print('%d, %10.3f, %10.3f' % (i, list_del_time, dict_del_time))
     
