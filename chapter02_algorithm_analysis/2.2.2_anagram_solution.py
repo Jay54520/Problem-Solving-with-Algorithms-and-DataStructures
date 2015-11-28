caf30155fa422c9f4eda5a3dbfc8f45623a064e9
Simon <simin_jie@163.com>
2015-11-28 10:20:25 +0800

    Angram soulution4 Count and compare  O(n)

--- chapter02_algorithm_analysis/2.2.2_anagram_solution.py
+++ chapter02_algorithm_analysis/2.2.2_anagram_solution.py
@@ -41,3 +41,46 @@ def angram_solution2(str1, str2):
 print(angram_solution1('abcd', 'dcba'))
    
 # angram_solution3 generate all possibilities, compare   
+
+# ord() Given a string representing one Unicode character, 
+# return an integer representing the Unicode code point of that character.
+
+# Count with 26 characters and compare
+def angram_solution4(str1, str2):
+    # 26 个字母每个出现的次数，　list[index] 用 ord(pos) - ord('a') 表示
+    list1 = [0] * 26
+    list2 = [0] * 26
+    
+    # 计算字母出现的次数
+    for character in str1:
+        pos = ord(character) - ord('a')
+        list1[pos] += 1
+        
+    for character in str2:
+        pos = ord(character) - ord('a')
+        list2[pos] += 1 
+
+    # 判断两个 list 是否相等
+    # 这里使用 while j < 26 比使用 for 迭代 list1 要好
+    j = 0
+    match = True
+    while j < 26 and match:
+        if list1[j] == list2[j]:
+            match = True
+            j += 1
+        else:
+            match = False
+            
+    return match 
+    
+    # 使用 for 迭代  O(n)
+    # for i in range(list1):
+        # if list1[i] == list2[i]:
+            # match = True
+        # else:
+            # match = False 
+            # break        
+
+print(angram_solution4('abcd', 'dcba'))            
+    
+    
