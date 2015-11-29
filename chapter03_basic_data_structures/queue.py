8b225c25b452acf7e6f467b3ba58b326dd168a94
Simon <simin_jie@163.com>
2015-11-29 21:23:59 +0800

    hot_potato and add self in __init__ and items to self. in queue.py

--- chapter03_basic_data_structures/queue.py
+++ chapter03_basic_data_structures/queue.py
@@ -1,17 +1,18 @@
 class Queue():
-    def __init__():
+    def __init__(self):
         self.items = []
         
     def enqueue(self, item):
-        self.item.insert(0, item)
+        self.items.insert(0, item)
         
-    def deque(self):
-        return self.pop()
+    def dequeue(self):
+        return self.items.pop()
         
-    def is_empty():
-        return len(self.item) == 0
+    def is_empty(self):
+        # 不使用 len == 0，减少计算
+        return self.items == []
         
-    def size():
-        return len(self.item)
+    def size(self):
+        return len(self.items)
         
         
