2e749d53603f09433ed7de6872487133166ab358
Simon <simin_jie@163.com>
2015-11-29 22:59:44 +0800

    Simulate printing tasks

--- chapter03_basic_data_structures/3.5.7_printing_tasks.py
+++ chapter03_basic_data_structures/3.5.7_printing_tasks.py
@@ -37,3 +37,35 @@ class Task:
     def wait_time(self, current_time):
         return current_time - self.timestamp
         
+def simulation(num_seconds, pages_per_minute):
+    lab_printer = Printer(pages_per_minute)
+    print_queue = Queue()
+    waiting_times = []
+    
+    for current_second in range(num_seconds):
+        if new_print_task():
+            task = Task(current_second)
+            print_queue.enqueue(task)
+            
+        if (not lab_printer.busy()) and (not print_queue.is_empty()):
+            next_task = print_queue.dequeue()
+            # 与 pages_per_minute 有没有关？ 这里怎么计算的秒数，我又没有经过那么长时间
+            waiting_times.append(next_task.wait_time(current_second))
+            lab_printer.start_next(next_task)
+        
+        # 减去打印时间 1s 判断是否为空闲 通过剩余时间
+        lab_printer.tick()
+    
+    average_wait = sum(waiting_times) / len(waiting_times)
+    print("Average Wait %6.2f secs %3d tasks remaining."
+        % (average_wait, print_queue.size()))
+            
+def new_print_task():
+    if random.randrange(1, 181) == 180:
+        return True
+    else:
+        return False
+        
+for i in range(10):
+    simulation(3600, 5)
+        
