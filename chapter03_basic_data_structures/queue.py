d2e639baf6454e4e2a628b42f65d83138bc778ab
Simon <simin_jie@163.com>
2015-11-29 20:52:16 +0800

    Class Queue

---
+++ chapter03_basic_data_structures/queue.py
@@ -0,0 +1,17 @@
+class Queue():
+    def __init__():
+        self.items = []
+        
+    def enqueue(self, item):
+        self.item.insert(0, item)
+        
+    def deque(self):
+        return self.pop()
+        
+    def is_empty():
+        return len(self.item) == 0
+        
+    def size():
+        return len(self.item)
+        
+        
