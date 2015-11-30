467a1c62c6bd45651e29d3999987687d9f85ad9d
Simon <simin_jie@163.com>
2015-11-30 11:28:31 +0800

    palindrome_checker uses Deque

---
+++ chapter03_basic_data_structures/3.6.4_palindrome_checker.py
@@ -0,0 +1,19 @@
+from deque import Deque
+
+def pal_checker(a_string):
+    char_deque = Deque()
+    
+    for ch in a_string:
+        char_deque.add_front(ch)
+    
+    match = True
+    while char_deque.size() > 1 and match:
+        if char_deque.remove_front() == char_deque.remove_rear():
+            match = True
+        else:
+            match = False 
+    
+    return match
+    
+print(pal_checker('maddam'))
+print(pal_checker('abcd'))
