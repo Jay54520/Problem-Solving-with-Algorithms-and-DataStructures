c3f9da3e45467cce41a18235460e5b53e3f7f505
Simon <simin_jie@163.com>
2015-11-30 22:35:07 +0800

    python 处理递归的方式

--- chapter04_recursion/4.2.3_num_to_str.py
+++ chapter04_recursion/4.2.3_num_to_str.py
@@ -3,7 +3,29 @@ def to_str(n, base):
     if n < base:
         return convert_string[n]
     else:
-        # base 转换需要翻过来 比如 stack
+        # base 杞崲闇�瑕佺炕杩囨潵 姣斿 stack
         return to_str(n // base, base) + convert_string[n % base]
         
 print(to_str(1453, 16))        
+
+from stack import Stack
+
+
+# python 鎬庢牱澶勭悊 閫掑綊
+def to_str2(n, base):
+    r_stack = Stack()
+    convert_string = '0123456789ABCDEF'
+    while n > 0:
+        if n < base:
+            r_stack.push(convert_string[n])
+        else:
+            r_stack.push(convert_string[n % base])
+        n = n // base
+    
+    res = ""
+    while not r_stack.is_empty():
+        res = res + r_stack.pop()
+    return res
+    
+print(to_str2(1453, 16))
+            
