f0e9aee8be543e93ed210ce1790c1de83cc20612
Simon <simin_jie@163.com>
2015-11-30 11:24:30 +0800

    Class Deque

---
+++ chapter03_basic_data_structures/deque.py
@@ -0,0 +1,23 @@
+class Deque():
+    def __init__(self):
+        self.items = []
+        
+    def add_front(self, item):
+        self.items.append(item)
+        
+    def add_rear(self, item):
+        self.items.insert(0, item)
+        
+    def remove_front(self):
+        return self.items.pop()
+        
+    def remove_rear(self):
+        return self.items.pop(0)
+        
+    def is_empty(self):
+        return self.items == []
+        
+    def size(self):
+        return len(self.items)
+        
+        
