4fee72227cd1f2c4071cab17159bd4ffa44f68af
Simon <simin_jie@163.com>
2015-12-02 16:58:08 +0800

    binary search

---
+++ chapter05_sorting_and_searching/5.2.2_binary_search.py
@@ -0,0 +1,38 @@
+def binary_search(order_list, search_num):
+    first = 0
+    last = len(order_list) - 1
+    found = False
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if order_list[midpoint] == search_num:
+            found = True
+        else:
+            if midpoint > search_num:
+                last -= 1
+            else:
+                first += 1
+    
+    return found
+
+def binary_search_recursive(order_list, search_num):
+    if len(order_list) == 0:
+        return False
+    else:
+        midpoint = len(order_list) // 2 
+        if order_list[midpoint] == search_num:
+            return True
+        elif order_list[midpoint] > search_num:
+            # 递归引用不能少了 return 
+            return binary_search_recursive(order_list[:midpoint - 1], search_num)
+        else:
+            return binary_search_recursive(order_list[midpoint + 1:], search_num)
+    
+test_list = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print(binary_search(test_list, 3))
+print(binary_search(test_list, 13))    
+
+print(binary_search_recursive(test_list, 3))
+print(binary_search_recursive(test_list, 13))
+        
+        
+    
