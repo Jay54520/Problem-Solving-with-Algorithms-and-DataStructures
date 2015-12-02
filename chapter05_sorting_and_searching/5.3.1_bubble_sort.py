48dbe4232c704444b500f4fc5ba07d87d56f90a6
Simon <simin_jie@163.com>
2015-12-02 22:33:27 +0800

    排序完毕后结束

--- chapter05_sorting_and_searching/5.3.1_bubble_sort.py
+++ chapter05_sorting_and_searching/5.3.1_bubble_sort.py
@@ -1,15 +1,16 @@
 def bubble_sort(a_list):
     length = len(a_list)
     pairs = length - 1
+    exchanges = True
     
-    while pairs > 1:
+    while pairs > 1 and exchanges:
+        # 如果下面没有变动，则说明已经排序完毕
+        exchanges = False
         for element in range(pairs):
             if a_list[element] > a_list[element+1]:
+                exchanges = True
                 # simultaneous 减少占用空间
-                a_list[element], a_list[element+1] = a_list[element+1], a_list[element]
-                # temp = a_list[element]
-                # a_list[element] = a_list[element+1]
-                # a_list[element+1] = temp
+                a_list[element], a_list[element+1] = a_list[element+1], a_list[element]                
         pairs -= 1    
     return a_list
     
