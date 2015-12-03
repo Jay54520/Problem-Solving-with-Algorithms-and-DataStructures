2115dd37ab21f5027db3d74c538376536d690415
Simon <simin_jie@163.com>
2015-12-03 21:36:11 +0800

    quicksort no additional memory

---
+++ chapter05_sorting_and_searching/5.3.6_quick_sort_no_additional_memory.py
@@ -0,0 +1,23 @@
+def partition(array, begin, end):
+    pivot = begin
+    for i in range(begin+1, end+1):
+        if array[i] <= array[begin]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+    array[pivot], array[begin] = array[begin], array[pivot]
+    return pivot
+
+
+def quicksort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+    if begin >= end:
+        return
+    pivot = partition(array, begin, end)
+    quicksort(array, begin, pivot-1)
+    quicksort(array, pivot+1, end)
+    return array
+    
+array = [97, 200, 100, 101, 211, 107]
+print(quicksort(array))
+# array -> [97, 100, 101, 107, 200, 211]    
