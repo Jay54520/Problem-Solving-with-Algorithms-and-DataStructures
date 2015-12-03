d465677df72ef496a0100a5680cbd0cc0f71dc23
Simon <simin_jie@163.com>
2015-12-03 09:06:38 +0800

    Shell sort between O(n)--O(n^2)

---
+++ chapter05_sorting_and_searching/5.3.4_shell_sort.py
@@ -0,0 +1,23 @@
+def shell_sort(a_list):
+    sublist_count = len(a_list) // 2
+    while sublist_count > 0:
+        for start_position in range(sublist_count):
+            gap_insertion_sort(a_list, start_position, sublist_count)
+            
+        print("After increments of size", sublist_count, "The list is", a_list)
+        
+        sublist_count = sublist_count // 2
+        
+def  gap_insertion_sort(a_list, start_position, gap):
+    for i in range(start_position + gap, len(a_list)):
+        while i >= gap and a_list[i] < a_list[i - gap]:
+            a_list[i - gap], a_list[i] = a_list[i], a_list[i - gap]
+    
+    return a_list
+    
+a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+shell_sort(a_list)
+print(a_list)
+            
+    
+        
