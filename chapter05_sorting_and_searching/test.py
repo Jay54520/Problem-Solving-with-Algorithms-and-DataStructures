9a702f014f2235a648e85b32aba75f53c7976983
Simon <simin_jie@163.com>
2015-12-04 09:26:28 +0800

    binary search

--- chapter05_sorting_and_searching/test.py
+++ chapter05_sorting_and_searching/test.py
@@ -1,23 +1,39 @@
+def binary_search(a_ascending_list, num):
+    first = 0
+    last = len(a_ascending_list) - 1
+    found = False 
+    
+    while first <= last and not found:
+        mid = (first + last) // 2
+        if a_ascending_list[mid] == num:
+            found = True
+        else:
+            if a_ascending_list[mid] > num:
+                last = mid - 1
+            else:
+                first = mid + 1 
+                
+    return found
+    
+test_list = [0, 1, 2, 8, 13, 17, 19, 32, 42]
+print(binary_search(test_list, 3))
+print(binary_search(test_list, 13))    
 
-
-def sort(array=[12,4,5,6,7,3,1,15]):
-    less = []
-    equal = []
-    greater = []
-
-    if len(array) > 1:
-        pivot = array[0]
-        for x in array:
-            if x < pivot:
-                less.append(x)
-            if x == pivot:
-                equal.append(x)
-            if x > pivot:
-                greater.append(x)
-        # Don't forget to return something!
-        return sort(less)+equal+sort(greater)  # Just use the + operator to join lists
-    # Note that you want equal ^^^^^ not pivot
-    else:  # You need to hande the part at the end of the recursion - when you only have one element in your array, just return the array.
-        return array
-
-print(sort())
+def binary_search_wrong(a_ascending_list, num):    
+    found = False
+    half = len(a_ascending_list) // 2
+    while len(a_ascending_list) > 1 and not found:
+        if a_ascending_list[half] == num:
+            found = True
+        elif a_ascending_list[half] < num:
+            binary_search(a_ascending_list[half + 1:], num)
+        else:
+            binary_search(a_ascending_list[:half], num)
+        
+    if len(a_ascending_list) == 1:
+        if a_ascending_list[0] == num:
+            return True
+        else:
+            return False
+    
+    return found
