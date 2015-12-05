436097654122b99dd66516256678d3f297052817
Simon <simin_jie@163.com>
2015-12-05 18:53:24 +0800

    Add put method in BinarySearchTree

--- chapter06_trees_and_tree_algorithms/binary_search_tree.py
+++ chapter06_trees_and_tree_algorithms/binary_search_tree.py
@@ -6,6 +6,30 @@ class BinarySearchTree:
     def length(self):
         return self.size 
         
+    def put(self, key, val):
+        if self.root:
+            self._put(key, val, self.root)
+        else:
+            self.root = TreeNode(key, val)
+            
+    def _put(self, key, val, current_node):
+        if key < current_node.key:
+            if current_node.has_left_child():
+                self._put(key, val, current_node.left_child)
+            else:
+                current_node.left_child = TreeNode(key, val,
+                    parent=current_node)
+        
+        else:
+            if current_node.has_right_child():
+                self._put(key, val, current_node.right_child)
+            else:
+                current_node.right_child = TreeNode(key, val,
+                    parent=current_node)
+                    
+    def __setitem__(self, k, v):
+        self.put(k, v)
+        
     def __len__(self):
         return self.size 
         
