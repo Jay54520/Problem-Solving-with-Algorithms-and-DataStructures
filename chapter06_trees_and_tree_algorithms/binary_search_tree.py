f37a7dec0729b9fc369e11c7be7b2c77f1e6deaf
Simon <simin_jie@163.com>
2015-12-05 21:51:49 +0800

    Delete complicated

--- chapter06_trees_and_tree_algorithms/binary_search_tree.py
+++ chapter06_trees_and_tree_algorithms/binary_search_tree.py
@@ -1,3 +1,90 @@
+class TreeNode:
+    def __init__(self, key, val, left = None, right = None, parent = None):
+        self.key = key
+        self.payload = val
+        self.left_child = left
+        self.right_child = right
+        self.parent = parent 
+        
+    def has_left_child(self):
+        return self.left_child
+        
+    def has_right_child(self):
+        return self.right_child
+        
+    def is_left_child(self):
+        # 存在 parent 需要注意
+        return self.parent and self.parent.left_child == self 
+        
+    def is_right_child(self):
+        return self.parent and self.parent.right_child == self
+        
+    def is_root(self):
+        return not self.parent 
+    
+    def is_leaf(self):
+        return not (self.left_child or self.right_child)
+        
+    def has_any_children(self):
+        return self.right_child or self.left_child
+        
+    def has_both_children(self):
+        return self.right_child and self.left_child
+        
+    def replace_node_data(self, key, value, lc, rc):
+        self.key = key 
+        self.payload = value 
+        self.left_child = lc 
+        self.right_child = rc 
+        if self.has_left_child():
+            self.left_child.parent = self
+        if self.has_right_child():
+            self.right_child.parent = self 
+
+    def find_successor(self):
+        succ = None 
+        if self.has_right_child():
+            succ = self.right_child.find_min()
+        else:
+            if self.parent:
+                if self.is_left_child():
+                    succ = self.parent
+                else:
+                    self.parent.right_child = None
+                    succ = self.parent.find_successor()
+                    self.parent.right_child = self 
+                    
+        return succ 
+        
+    def find_min(self):
+        current = self
+        while current.has_left_child():
+            current = current.left_child
+        return current 
+        
+    def splice_out(self):
+        if self.is_leaf():
+            if self.is_left_child():
+                self.parent.left_child = self.left_child
+            else:
+                self.parent.right_child = self.left_child
+        elif self.has_any_children():
+            if self.has_left_child():
+                if self.is_left_child():
+                    self.parent.left_child = self.left_child
+                else:
+                    self.parent.right_child = self.left_child
+                # 有 children 就要告诉它它的爹是谁， 
+                # 上面的 leaf 没有 children, 就只需要告诉它爷爷它的儿子变了
+                self.left_child.parent = self.parent 
+            else:
+                if self.is_left_child():
+                    self.parent.left_child = self.right_child
+                else: 
+                    self.parent.right_child = self.right_child
+                self.right_child.parent = self.parent
+            
+    
 class BinarySearchTree:
     def __init__(self):
         self.root = None
@@ -60,6 +147,66 @@ class BinarySearchTree:
             return True 
         else:
             return False 
+            
+    def delete(self, key):
+        if self.size > 1:
+            node_to_remove = self._get(key, self.root)
+            if node_to_remove:
+                self.remove(node_to_remove)
+                self.size -= 1
+            else:
+                raise KeyError("Error, key not in tree")
+        elif self.size == 1 and self.root.key == key:
+            self.root = None 
+            self.size -= 1
+        else:
+            raise KeyError("Error, key not in tree")
+            
+    def __delitem__(self, key):
+        self.delete(key)
+        
+    def remove(self, current_node):
+        if current_node.is_leaf():
+            if current_node == current_node.parent.left_child:
+                current_node.parent.left_child = None
+            else:
+                current_node.parent.right_child = None 
+        
+        elif current_node.has_both_children():
+            succ = current_node.find_successor()
+            succ.splice_out()
+            current_node.key = succ.key 
+            current_node.payload = succ.payload
+        
+        else: # this node has one child
+            if current_node.has_left_child():
+                if current_node.is_left_child():
+                    # 有点绕 告诉孩子它的爷爷是谁，告诉它爷爷它孩子是谁
+                    current_node.left_child.parent = current_node.parent
+                    current_node.parent.left_child = current_node.left_child
+                elif current_node.is_right_child():
+                    current_node.parent.right_child = current_node.left_child
+                    current_node.left_child.parent = current_node.parent 
+                else: # current_node 没 parent, 删除后儿子顶上 把 root 替换为儿子的值
+                    current_node.replace_node_data(current_node.left_child.key,
+                                    current_node.left_child.payload,
+                                    current_node.left_child.left_child,
+                                    current_node.left_child.right_child)
+                                    
+            # current_node 有右边的孩子
+            else:                         
+                if current_node.is_left_child():
+                    current_node.right_child.parent = current_node.parent
+                    current_node.parent.left_child = current_node.right_child
+                elif current_node.is_right_child():
+                    current_node.right_child.parent = current_node.parent
+                    current_node.parent.right_child = current_node.right_child
+                else:
+                    current_node.replace_node_data(current_node.right_child.key,
+                                    current_node.right_child.payload,
+                                    current_node.right_child.left_child,
+                                    current_node.right_child.right_child)
+                    
     
     def __len__(self):
         return self.size 
@@ -67,47 +214,6 @@ class BinarySearchTree:
     def __iter__(self):
         return self.root.__iter__()
         
-class TreeNode:
-    def __init__(self, key, val, left = None, right = None, parent = None):
-        self.key = key
-        self.payload = val
-        self.left_child = left
-        self.right_child = right
-        self.parent = parent 
-        
-    def has_left_child(self):
-        return self.left_child
-        
-    def has_right_child(self):
-        return self.right_child
-        
-    def is_left_child(self):
-        # 存在 parent 需要注意
-        return self.parent and self.parent.left_child == self 
-        
-    def is_right_child(self):
-        return self.parent and self.parent.right_child == self
-        
-    def is_root(self):
-        return not self.parent 
-    
-    def is_leaf(self):
-        return not (self.left_child or self.right_child)
-        
-    def has_any_children(self):
-        return self.right_child or self.left_child
-        
-    def has_both_children(self):
-        return self.right_child and self.left_child
-        
-    def replace_node_data(self, key, value, lc, rc):
-        self.key = key 
-        self.payload = value 
-        self.left_child = lc 
-        self.right_child = rc 
-        if self.has_left_child():
-            self.left_child.parent = self
-        if self.has_right_child():
-            self.right_child.parent = self 
+
             
        
