4a5c42cacaad1104b45087895ed36d90657e4b20
Simon <simin_jie@163.com>
2015-12-05 15:58:32 +0800

    Class BinaryTree

---
+++ chapter06_trees_and_tree_algorithms/binarytree.py
@@ -0,0 +1,33 @@
+class BinaryTree:
+    def __init__(self, root):
+        self.key = root 
+        self.left_child = None 
+        self.right_child = None 
+        
+    def insert_left(self, new_node):
+        t = BinaryTree(new_node)
+        if self.left_child == None:
+            self.left_child = t 
+        else:
+            t.left_child = self.left_child
+            self.left_child = t 
+    
+    def insert_right(self, new_node):
+        t = BinaryTree(new_node)
+        if self.right_child == None:
+            self.right_child = t 
+        else:
+            t.right_child = self.left_child
+            self.right_child = t 
+            
+    def get_right_child(self):
+        return self.right_child
+        
+    def get_left_child(self):
+        return self.left_child
+        
+    def set_root_val(self, obj):
+        self.key = obj 
+        
+    def get_root_val(self):
+        return self.key  
