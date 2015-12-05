74a2600924d8bce540f802e5d4f390b9b03fded0
Simon <simin_jie@163.com>
2015-12-05 19:00:52 +0800

    get and __getitem__

--- chapter06_trees_and_tree_algorithms/binary_search_tree.py
+++ chapter06_trees_and_tree_algorithms/binary_search_tree.py
@@ -30,6 +30,32 @@ class BinarySearchTree:
     def __setitem__(self, k, v):
         self.put(k, v)
         
+    def get(self, key):
+        if self.root:
+            res = self._get(key, self.root)
+            if res:
+                return res.payload
+            else:
+                return None 
+                
+        else:
+            return None
+    
+    def _get(self, key, current_node):
+        if not current_node:
+            return None 
+        elif current_node.key == key:
+            return current_node
+        elif key < current_node.key:
+            return self._get(key, current_node.left_child)
+        else:
+            return self._get(key, current_node.right_child)
+            
+    def __getitem__(self, key):
+        return self.get(key)
+        
+    # in operation 
+    
     def __len__(self):
         return self.size 
         
