e0a0b89cd756a42427a74c2a9cfc9a5154ba1b2c
Simon <simin_jie@163.com>
2015-12-05 13:19:06 +0800

    def build_parse_tree

---
+++ chapter06_trees_and_tree_algorithms/6.6_build_parse_tree.py
@@ -0,0 +1,33 @@
+from stack import Stack 
+from BinHeap import BinHeap as BinaryTree
+
+def build_parse_tree(fp_exp):
+    fp_list = fp_exp.split()
+    p_stack = Stack()
+    e_tree = BinaryTree('')
+    p_stack.push(e_tree)
+    current_tree = e_tree 
+    
+    for i in fp_list:
+        if i == '(':
+            current_tree.insert_left('')
+            p_stack.push(current_tree)
+            current_tree = current_tree.get_left_child()
+        # operands 
+        elif i not in ['+', '-', '*', '/', ')']:
+            current_tree.set_root_val(int(i))
+            parent = p_stack.pop()
+            current_tree = parent 
+        elif i in ['+', '-', '*', '/']:
+            current_tree.set_root_val(i)
+            current_tree.insert_right('')
+            p_stack.push(current_tree)
+            current_tree = current_tree.get_right_child()
+        elif i == ')':
+            current_tree = p_stack.pop()
+        else:
+            raise ValueError
+    return e_tree 
+    
+pt = build_parse_tree("( ( 10 + 5 ) * 3 )")
+pt.postorder()
