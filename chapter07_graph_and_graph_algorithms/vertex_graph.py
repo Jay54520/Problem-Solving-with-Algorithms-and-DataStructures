99bc9e6dfe25897ac574dfb1dece195e88150d84
Simon <simin_jie@163.com>
2015-12-06 18:16:49 +0800

    Some methods about color, distance, predecessor,distance and finish in Vertex

--- chapter07_graph_and_graph_algorithms/vertex_graph.py
+++ chapter07_graph_and_graph_algorithms/vertex_graph.py
@@ -1,13 +1,49 @@
+import sys 
+import os 
+import unittest
+
 class Vertex:
     def __init__(self, key):
         self.id = key
         self.connectedTo = {}
+        self.color = 'white'
+        self.dist = sys.maxsize
+        self.pred = None
+        self.disc = 0
+        self.fin = 0
         
     def addNeighbor(self, nbr, weight=0):
         self.connectedTo[nbr] = weight
+    
+    def setColor(self, color):
+        self.color = color 
         
-    def __str__(self):
-        return str(self.id) + 'connectedTo: ' + str([x.id for x in self.connectedTo])
+    def setDistance(self, d):
+        self.dist = d
+        
+    def setPred(self, p):
+        self.pred = p 
+        
+    def setDiscovery(self, dtime):
+        self.disc = dtime 
+        
+    def setFinish(self, ftime):
+        self.fin = ftime 
+        
+    def getFinish(self):
+        return self.fin 
+    
+    def getDiscovery(self):
+        return self.disc 
+        
+    def getPred(self):
+        return self.pred
+        
+    def getDistance(self):
+        return self.dist
+        
+    def getColor(self):
+        return self.color        
         
     def getConnections(self):
         return self.connectedTo.keys()
@@ -18,7 +54,11 @@ class Vertex:
     def getWeight(self, nbr):
         return self.connectedTo[nbr]
         
-
+    def __str__(self):
+        return str(self.id) + ":color " + self.color + ":disc " + \
+                    str(self.disc) + ":fin " + str(self.fin) + ":dist " + \
+                    str(self.dist) + ":pred \n\t[" + str(self.pred)+ "]\n"
+    
 class Graph:
     def __init__(self):
         self.vertList = {}
