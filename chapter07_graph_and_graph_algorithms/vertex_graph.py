aee45ed4044f625c20fdcc5ab3a188345739d956
Simon <simin_jie@163.com>
2015-12-06 10:10:11 +0800

    Test the Graph and Vertex

--- chapter07_graph_and_graph_algorithms/vertex_graph.py
+++ chapter07_graph_and_graph_algorithms/vertex_graph.py
@@ -52,4 +52,20 @@ class Graph:
     def __iter__(self):
         return iter(self.vertList.values())
         
-        
+if __name__ == '__main__':
+    g = Graph()
+    for i in range(6):
+        g.addVertex(i)
+    print(g.vertList)    
+    g.addEdge(0,1,5)
+    g.addEdge(0,5,2)
+    g.addEdge(1,2,4)
+    g.addEdge(2,3,9)
+    g.addEdge(3,4,7)
+    g.addEdge(3,5,3)
+    g.addEdge(4,0,1)
+    g.addEdge(5,4,8)
+    g.addEdge(5,2,1)
+    for v in g:
+        for w in v.getConnections():
+            print("( %s , %s )" % (v.getId(), w.getId()))
