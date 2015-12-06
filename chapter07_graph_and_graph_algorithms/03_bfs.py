29989feda8af166816a508f1ecc1f828815da19c
Simon <simin_jie@163.com>
2015-12-06 11:13:45 +0800

    traverse method to print

--- chapter07_graph_and_graph_algorithms/03_bfs.py
+++ chapter07_graph_and_graph_algorithms/03_bfs.py
@@ -15,4 +15,13 @@ def bfs(g, start):
                 nbr.setPred(currentVert)
                 vertQueue.enqueue(nbr)
         currentVert.setColor('black')
+        
+def traverse(y):
+        x = y
+        while (x.getPred()):
+            print(x.getId())
+            x = x.getPred()
+        print(x.getId())
+        
+traverse(g.getVertex('sage'))
 
