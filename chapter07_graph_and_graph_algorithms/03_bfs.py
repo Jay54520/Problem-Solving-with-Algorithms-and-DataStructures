3290b135202bb04105a7d03a8ead65912c728d3f
Simon <simin_jie@163.com>
2015-12-06 11:12:28 +0800

    bfs

---
+++ chapter07_graph_and_graph_algorithms/03_bfs.py
@@ -0,0 +1,18 @@
+from queue import Queue 
+from vertex_graph import Vertex, Graph
+
+def bfs(g, start):
+    start.setDistance(0)
+    start.setPred(None)
+    vertQueue = Queue()
+    vertQueue.enqueue(start)
+    while (vertQueue.size() > 0):
+        currentVert = vertQueue.dequeue()
+        for nbr in currentVert.getConnections():
+            if (nbr.getColor() == 'whilte'):
+                nbr.setColor('gray')
+                nbr.setDistance(currentVert.getDistance() + 1)
+                nbr.setPred(currentVert)
+                vertQueue.enqueue(nbr)
+        currentVert.setColor('black')
+
