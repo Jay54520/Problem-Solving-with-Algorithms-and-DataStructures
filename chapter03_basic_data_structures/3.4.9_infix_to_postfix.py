a5ac87b3700c67deb6a52db874374f88619dfedc
Simon <simin_jie@163.com>
2015-12-08 15:25:33 +0800

    infix_to_postfix

--- chapter03_basic_data_structures/3.4.9_infix_to_postfix.py
+++ chapter03_basic_data_structures/3.4.9_infix_to_postfix.py
@@ -1,41 +1,35 @@
 from stack import Stack
-def infix_to_postfix(infix_expression):
-    token_list = infix_expression.split()
-    precedence = {}
-    precedence["("] = 1
-    precedence["+"] = 2
-    precedence["-"] = 2
-    precedence["*"] = 3
-    precedence["/"] = 3
-    precedence["^"] = 4
-    operator_stack = Stack()
-    postfix_list = []
-    
-    for token in token_list:
-        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or \
-            token in "0123456789":
-            postfix_list.append(token)
-        elif token == '(':
-            operator_stack.push(token)
-        elif token == ')':
-            # 弹出最后传入的 token 
-            top_token = operator_stack.pop()
-            while top_token != '(':
-                # 这样括号里没有优先级比较                
-                postfix_list.append(top_token)
-                top_token = operator_stack.pop()
-        # 比较 + - * / 优先级
-        else:
-            while (not operator_stack.is_empty()) and \
-                (precedence[operator_stack.peek()] >= precedence[token]):
-                postfix_list.append(operator_stack.pop())
-            operator_stack.push(token)
-        
-    while not operator_stack.is_empty():
-        postfix_list.append(operator_stack.pop())
-    return " ".join(postfix_list)
+import ipdb
 
-print(infix_to_postfix("A * B + C * D"))
-print(infix_to_postfix("( A + B + C ^ ( 4 - 2 ) ) * C - ( D - E ) * ( F + G )"))
-            
+def infix_to_postfix(infix_string):
+    infix_list = infix_string.split()
+    s = Stack()
+    output = []
+    p = {'(':0, ')':0, '+':1, '-':1, '*':2, '/':2}
         
+    for e in infix_list:
+        if e in 'ABCDEFGHIJKLMNOPQRST' or e in '012346789':
+            output.append(e)
+        elif e == '(':
+            s.push(e)
+        elif e == ')':
+            while not s.peek() == '(':
+                output.append(s.pop())
+            s.pop()
+        else:
+            if not s.is_empty():
+                if p[e] <= p[s.peek()]:
+                    output.append(s.pop())
+            s.push(e)
+    
+    while not s.is_empty():
+        output.append(s.pop())
+        
+    result = ""
+    for e in output:
+        result += e 
+        
+    return result 
+    
+print(infix_to_postfix("A * B + C * D"))
+print(infix_to_postfix("( A + B + C ) * C - ( D - E ) * ( F + G )"))    
