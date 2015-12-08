2835e1aba40355f6f9612f12deab195164a9f7dd
Simon <simin_jie@163.com>
2015-12-08 12:57:30 +0800

    check_par

--- chapter03_basic_data_structures/3.4.3_balance_symbols(a_general_case).py
+++ chapter03_basic_data_structures/3.4.3_balance_symbols(a_general_case).py
@@ -1,35 +1,27 @@
 from stack import Stack
 import ipdb
 
-# 括号类型是否匹配
+def check_par(symbol_string):
+    s = Stack()
+    for a_symbol in symbol_string:
+        if a_symbol in '{[(':
+            s.push(a_symbol)
+        elif a_symbol in '}])':
+            if s.is_empty():
+                return False 
+            if not match(s.pop(), a_symbol):
+                return False
+    
+    if not s.is_empty():
+        return False 
+        
+    return True 
+    
 def match(open, close):
-    opens = '([{'
-    closes = ')]}'
-    # 返回 bool 值，直接　return 不需要再判断
+    opens = '{[('
+    closes = '}])'
+    
     return opens.index(open) == closes.index(close)
     
-# 括号之间匹配 后进先出 '[[{{(())}}]]', 不匹配的'([)]'，所以还需要加上类型匹配
-def par_checker(symbol_string):
-    s = Stack()
-    index = 0
-    balanced = True
-    while index < len(symbol_string) and balanced:
-        if symbol_string[index] in '([{':
-            s.push(symbol_string[index])
-        else:
-            if s.is_empty():
-                balanced = False
-            else:
-                top = s.pop() # 弹出上一个储存的 '([{' 中的一个
-                if not match(top, symbol_string[index]):
-                    balanced = False
-        index += 1
-        
-    if s.is_empty() and balanced:
-        return True
-    else:
-        return False
-        
-print(par_checker('[[{{(())}}]]'))        
-print(par_checker('([)]'))
-    
+print(check_par('[[{{(())}}]]'))        
+print(check_par('([)]'))    
