b5ebc02389fada909ebeff1b327e32955c40577b
Simon <simin_jie@163.com>
2015-12-08 16:07:40 +0800

    符号增加 ^

--- chapter03_basic_data_structures/3.4.9_infix_to_postfix.py
+++ chapter03_basic_data_structures/3.4.9_infix_to_postfix.py
@@ -5,10 +5,10 @@ def infix_to_postfix(infix_string):
     infix_list = infix_string.split()
     s = Stack()
     output = []
-    p = {'(':0, ')':0, '+':1, '-':1, '*':2, '/':2}
+    p = {'(':0, ')':0, '+':1, '-':1, '*':2, '/':2, '^':3}
         
     for e in infix_list:
-        if e in 'ABCDEFGHIJKLMNOPQRST' or e in '012346789':
+        if e in 'ABCDEFGHIJKLMNOPQRST' or e in '0123456789':
             output.append(e)
         elif e == '(':
             s.push(e)
@@ -29,4 +29,4 @@ def infix_to_postfix(infix_string):
     return result 
     
 print(infix_to_postfix("A * B + C * D"))
-print(infix_to_postfix("( A + B + C ) * C - ( D - E ) * ( F + G )"))    
+print(infix_to_postfix("5 * 3 ^ ( 4 - 2 )"))    
