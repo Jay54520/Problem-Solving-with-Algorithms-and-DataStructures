2c89c1792e7221401cfa0937ccc743f9f49faf05
Simon <simin_jie@163.com>
2015-12-08 13:00:56 +0800

    base_converter

--- chapter03_basic_data_structures/3.4.5_2_divide_by_base.py
+++ chapter03_basic_data_structures/3.4.5_2_divide_by_base.py
@@ -1,21 +1,20 @@
 from stack import Stack
 import ipdb
 
-def base_converter(decimal_number, base=2):
-    # 余数作为 index 与这个相对应 
-    digits = '0123456789ABCDEF'
-    reminder_stack = Stack()
-    while decimal_number > 0:
-        reminder = decimal_number % base
-        reminder_stack.push(reminder)
-        decimal_number = decimal_number // base
+def base_converter(n, base=2):
+    s = Stack()
+    remainders = '0123456789ABCDEF'
     
-    # 将 reminder_stack 转化为 string
-    new_string = ""
-    while not reminder_stack.is_empty():
-        new_string = new_string + digits[reminder_stack.pop()]
+    while n > 0:
+        remainder = remainders[n % base]
+        s.push(remainder)
+        n = n // base 
+        
+    result = ""
+    while not s.is_empty():
+        result += str(s.pop())
     
-    return new_string
+    return result 
     
 print(base_converter(42))
-print(base_converter(25, 16))
+print(base_converter(256, 16))    
